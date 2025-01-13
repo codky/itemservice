bae79658c4d84f153f34b18e000353d425f412e4
minseok kyeon <msky0912@gmail.com>
2025-01-13 13:16:25 +0900

    상품 등록폼 컨트롤러 뷰 개발

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +32,18 @@ public class BasicItemController {
         model.addAttribute("item", item);
 
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @ResponseBody
+    @PostMapping("/add")
+    public String save() {
+//        return "basic/addForm";
+        return "/add";
     }
 
     @PostConstruct
